e0c706bde50454394079dfd5f3f33c7c93d5a99b
Pavel Romanov <pavel.romanov101@gmail.com>
2019-11-25 16:11:17 +0200

    Add files via upload

---
+++ ExamPrep/TomAndJerry V2/Main.cpp
@@ -0,0 +1,127 @@
+#include "Point.h"
+#include "Room.h"
+#include "shortestPath.h"
+
+int main()
+{
+	//input WORKING
+	//int room_length, room_width;
+	//int JerryX, JerryY;
+	//int TomX, TomY;
+	//int furniture, paint;
+
+	//bool maze[5][5] = {
+	//	{0, 0, 0, 0, 1},
+	//	{1, 0, 1, 0, 1},
+	//	{1, 0, 1, 0, 1},
+	//	{1, 0, 1, 0, 1},
+	//	{1, 0, 1, 0, 0},
+	//};
+
+	//int counter = 1;
+	//ifstream file("input.txt");
+	//int num1, num2;
+	//while (file >> num1 >> num2)
+	//{
+	//	if (counter == 1) {
+	//		room_length = num1;
+	//		room_width = num2;
+	//	}
+
+	//	else if (counter == 2) {
+	//		TomX = num1;
+	//		TomY = num2;
+	//	}
+
+	//	else if (counter == 3) {
+	//		JerryX = num1;
+	//		JerryY = num2;
+	//	}
+
+	//	else if (counter == 4) {
+	//		furniture = num1;
+	//		paint = num2;
+	//	}
+	//	counter++;
+	//}
+
+	//Room room(room_length, room_width);
+
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	for (int j = 0; j < 5; j++)
+	//	{
+	//		room.arr[i][j].setVisited(maze[i][j]);
+	//		room.arr[i][j].setX(i);
+	//		room.arr[i][j].setY(j);
+	//	}
+	//}
+
+	//Point Tom(TomX, TomY, 0);
+	//Point Jerry(JerryX, JerryY, 0);
+
+	//cout << "Shortest Path from Tom to Jerry is: " << room.shortestPath(Tom, Jerry) << endl;
+	//shortestPath(Tom, Jerry);
+	//cout << endl;
+
+	//======================================================
+	//TESTING FURNITURE
+
+	int arr[3][3] = {
+		{0,0,0},
+		{0,0,0},
+		{0,0,0},
+	};
+
+	int line = 1;
+	fstream files("test.txt");
+	int startX, startY;
+	string inputs;
+	int y = 0;
+	int n1, n2;
+
+	while (!files.eof())
+	{
+		if (line == 1)
+		{
+			files >> n1 >> n2;
+			startX = n1;
+			startY = n2;
+		}
+		else 
+		{
+			vector<char> vector;
+			files >> inputs;
+			for (int i = 0; i < inputs.length(); i++)
+			{
+				vector.push_back(inputs[i]);
+				if (vector[i] == '.') {
+					arr[startX + y][startY + i] = 0;
+				}
+
+				else if (vector[i] == '1')
+				{
+					arr[startX + y][startY + i] = 1;
+				}
+
+				else if (vector[i] == '=') {
+
+					line = 0;
+					y = -1;
+					break;
+				}
+			}
+			y++;
+		}
+		line++;
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int  j = 0; j < 3; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
---
+++ ExamPrep/TomAndJerry V2/shortestPath.h
@@ -0,0 +1,70 @@
+#pragma once
+#include "Point.h"
+#define N 5
+
+vector<Point> path;
+Point Tom(0, 0, 0);
+Point Jerry(3, 3, 0);
+
+bool maze[N][N] = {
+	{0, 0, 0, 0, 1},
+	{1, 0, 1, 0, 1},
+	{1, 0, 1, 0, 1},
+	{1, 0, 1, 0, 1},
+	{1, 0, 1, 0, 0},
+};
+
+int sol[N][N];   
+
+bool isValidPlace(int x, int y) { 
+	if (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 0)
+		return true;
+	return false;
+}
+
+bool solveMaze(int x, int y) {
+
+	if (x == Jerry.getX() && y == Jerry.getY()) {       //when (x,y) is the bottom right room
+		sol[x][y] = 1;
+		return true;
+	}
+
+	if (isValidPlace(x, y) == true) {     //check whether (x,y) is valid or not
+		sol[x][y] = 1; //set 0, when it is valid place
+		if (solveMaze(x + 1, y) == true)       //find path by moving right direction
+			return true;
+		if (solveMaze(x, y + 1) == true)         //when x direction is blocked, go for bottom direction
+			return true;
+		sol[x][y] = 0;         //if both are closed, there is no path
+		return false;
+	}
+	return false;
+}
+
+bool shortestPath(Point Tom, Point Jerry) {
+
+	if (solveMaze(Tom.getX(), Tom.getY()) == false) {
+		cout << "There is no path";
+		return false;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (sol[i][j] == 1)
+			{
+				Point temp(i, j, 0);
+				path.push_back(temp);
+			}
+		}
+	}
+
+	for (int i = 0; i < path.size() - 1; i++)
+	{
+		cout << path[i] << " -> ";
+	}
+	cout << path[path.size() - 1];
+
+	return true;
+}
