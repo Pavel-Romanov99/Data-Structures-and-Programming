869a48962e30626df8ab33c97c970e51c325d9ea
Pavel Romanov <pavel.romanov101@gmail.com>
2019-12-06 16:46:46 +0200

    Update main.cpp

--- ExamPrep2/BinaryTree/main.cpp
+++ ExamPrep2/BinaryTree/main.cpp
@@ -3,10 +3,13 @@
 int main()
 {
 	BinaryTree<int> a(5);
-	a.insertNode(a.root,1);
-	a.insertNode(a.root,6);
+	a.insertNode(a.root, 1);
+	a.insertNode(a.root, 6);
 	a.insertNode(a.root, 8);
 	a.insertNode(a.root, 9);
+	a.inOrder(a.root);
 	cout << endl;
-	cout << a.height(a.root) << endl;
+	
+	cout << a.accumulate(a.root, product, 1) << endl;
+	cout << a.accumulate(a.root, addition, 0) << endl;
 }
